5ffbece15ae4df0f4ad3090febe827b5a0ab5526
AndreYamasaki <katsuya.tsu@gmail.com>
2021-05-12 17:56:45 -0400

    Initial Commit

---
+++ MarvelAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelAPI
+//
+//  Created by user191918 on 5/12/21.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
