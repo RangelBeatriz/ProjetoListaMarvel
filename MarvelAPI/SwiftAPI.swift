d79b1fd3b0ab34aa8b6460c1e215a17a18fcfea0
AndreYamasaki <katsuya.tsu@gmail.com>
2021-05-13 20:01:34 -0400

    URL request

--- MarvelAPI/SwiftAPI.swift
+++ MarvelAPI/SwiftAPI.swift
@@ -7,27 +7,30 @@
 
 import Foundation
 import Alamofire
-import UIKit
+import CryptoKit
 
 class SwiftAPI {
-    let baseURL = "http://gateway.marvel.com/v1/public/comics"
+    let baseURL = "http://gateway.marvel.com/v1/public/comics?"
     let privateKey = "e6fce4477364edd99c05b641b530673dc87f8008"
-    let apiKey = "ee6645130f54ce96d6d993660fa2be92"
+    let publicKey = "ee6645130f54ce96d6d993660fa2be92"
+    let ts = String(Date().timeIntervalSince1970)
     
-    
-    
-    typealias WebServiceResponse = ([[String: Any]]?, Error?) -> Void
-    
-    func urlRequest (_ url: URL, completion: @escaping WebServiceResponse) {
-        AF.request(url).validate().responseJSON {response in
-            if let error = response.error {
-                completion(nil, error)
-            } else if let jsonArray = response.result as? [[String: Any]] {
-                completion(jsonArray, nil)
-            } else if let jsonDict = response.result as? [String: Any] {
-                completion([jsonDict], nil)
-            }
+    public func apiRequest() {
+        let parameters = ["ts": ts, "hash": self.getMD5(), "apiKey" : publicKey]
+        
+        let request = AF.request(baseURL, parameters: parameters)
+        
+        request.responseJSON { (data) in
+            print(data)
         }
     }
+    
+    private func getMD5() -> String {
+        let apiData = ts + privateKey + publicKey
+        guard let data = apiData.data(using: .utf8) else {return String()}
+        let apiHash = Insecure.MD5.hash(data: data)
+        return apiHash.map {String(format: "%02hhx", $0)}.joined()
+    }
+
     
 }
