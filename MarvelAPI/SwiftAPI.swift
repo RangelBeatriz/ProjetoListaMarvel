ca69dcbd9a279810dce50dea5141a51bceff1d67
AndreYamasaki <katsuya.tsu@gmail.com>
2021-05-20 13:48:55 -0400

    Fixed some problems with tableview

--- MarvelAPI/SwiftAPI.swift
+++ MarvelAPI/SwiftAPI.swift
@@ -9,11 +9,18 @@ import Foundation
 import Alamofire
 import CryptoKit
 
+protocol ApiResponse {
+    func success()
+    func error()
+}
+
 class SwiftAPI {
     let baseURL = "http://gateway.marvel.com/v1/public/characters"
     let privateKey = "e6fce4477364edd99c05b641b530673dc87f8008"
     let publicKey = "ee6645130f54ce96d6d993660fa2be92"
     let ts = String(Date().timeIntervalSince1970)
+    typealias  charactersCallBack = (_ chars:[Character]?, _ status: Bool, _ message: String) -> Void
+    var callBack:charactersCallBack?
     
     public func apiRequest() {
             let parameters = ["ts": ts, "hash": self.getMD5(), "apikey" : publicKey]
@@ -29,8 +36,10 @@ class SwiftAPI {
                     let characters = try JSONDecoder().decode(MarvelAPIData.self, from: data)
                     
                     print("data == \(characters)")
+                    self.callBack?(characters.data.results, true, "")
                 } catch {
                     print("Error decoding == \(error)")
+                    self.callBack?(nil, false, error.localizedDescription)
                 }
             }
             
@@ -41,6 +50,10 @@ class SwiftAPI {
         guard let data = apiData.data(using: .utf8) else {return String()}
         let apiHash = Insecure.MD5.hash(data: data)
         return apiHash.map {String(format: "%02hhx", $0)}.joined()
+    }
+    
+    func completionHandler(callback: @escaping charactersCallBack) {
+        self.callBack = callback
     }
 
     
--- MarvelAPI/ViewController.swift
+++ MarvelAPI/ViewController.swift
@@ -6,26 +6,66 @@
 //
 
 import UIKit
-import CryptoKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
     
     @IBOutlet weak var characterImage: UIImageView!
     @IBOutlet weak var characterName: UILabel!
     @IBOutlet weak var characterDescription: UILabel!
     
+    @IBOutlet weak var tableChars: UITableView!
+    
+    
     
     
     var apiMarvel = SwiftAPI()
-
+    var characters = [Character]()
+    
+    
     override func viewDidLoad() {
+        tableChars.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         apiMarvel.apiRequest()
+        
+        apiMarvel.completionHandler { (characters, status, message) in
+            if status {
+                guard let _characters = characters else {return}
+                self.characters = _characters
+                self.tableChars.reloadData()
+            }
+            
+        }
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cellIdentifier")
+        }
+        let chars = characters[indexPath.row].name
+        cell?.textLabel?.text = chars
+        return cell!
+    }
+    
+    
 }
 
+extension ViewController: ApiResponse {
+    func success() {
+        tableChars.reloadData()
+    }
+    
+    func error() {
+        print("Error")
+    }
+}
